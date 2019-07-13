f4b22551033f77d2741b3c53e1c5f35897adeedd
John Pino <freelance@johnpino.me>
2019-07-13 10:50:10 -0500

    Improve mainContent

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
-    $('.post__container').slick({
+    $('.slider').slick({
         arrows: true,
         infinite: true,
-        //slidesToShow: 3,
-        //slidesToScroll: 3
+        variableWidth: true
     });
 });
